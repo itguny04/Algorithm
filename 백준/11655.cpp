f08eee6c448c3c398486a5608c54c9aa62d92e43
itguny04 <the2133@naver.com>
2022-03-14 09:44:28 +0900

    add

---
+++ 백준/11655.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void) {
+    int[26] uppercase;
+    int[26] lowercase;
+
+    return 0;
+}
---
+++ 백준/2440.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+    int N;
+
+    std::cin >> N;
+
+    for(int i=N; i>0; i--) {
+        for(int j=0; j<i; j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
