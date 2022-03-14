9aea5c4ae73ea4af2e3acc09618f41a0b77c2555
itguny04 <the2133@naver.com>
2022-03-14 10:53:36 +0900

    rot13

--- 백준/11655.cpp
+++ 백준/11655.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
 int main(void) {
-    int[26] uppercase;
-    int[26] lowercase;
+    std::string user_input;
+    std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+    std::getline(std::cin, user_input, '\n');
+  
+    for(int i=0; i<user_input.length(); i++) {
+        if('A' <= user_input[i] && user_input[i] <= 'M') {
+            user_input[i] = uppercase[(user_input[i]-'A')+13];  
+        } else if('N' <= user_input[i] && user_input[i] <= 'Z') {
+            user_input[i] = uppercase[(user_input[i]-'A')-13];
+        } else if('a' <= user_input[i] && user_input[i] <= 'm') {
+            user_input[i] = lowercase[(user_input[i]-'a')+13];
+        } else if('n' <= user_input[i] && user_input[i] <= 'z') {
+            user_input[i] = lowercase[(user_input[i]-'a')-13];
+        } else {
+            continue;
+        }
+    }
+    
+    std::cout << user_input;
 
     return 0;
 }
